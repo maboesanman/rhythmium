9c7a36b65f698cb15c29e51f6e77c0707e41988c
Mason Boeman <masonboeman@gmail.com>
2024-01-06 19:11:29 -0600

    clean up anonymize functions

--- cef_wrapper/src/lib.rs
+++ cef_wrapper/src/lib.rs
@@ -18,8 +18,11 @@ pub(crate) mod sys {
     include!(concat!(env!("OUT_DIR"), "/bindings_cpp.rs"));
 }
 
+mod anonymize;
 pub mod browser;
 pub mod browser_host;
+
+use anonymize::{anonymize, anonymize_mut};
 
 pub struct CefApp;
 
@@ -69,12 +72,13 @@ impl CefApp {
                 panic!("browser creation failed");
             }
         };
-        let (get_view_rect, get_view_rect_arg) = anonymize_get_view_rect(get_view_rect);
-        let (on_paint, on_paint_arg) = anonymize_on_paint(on_paint);
-        let (on_browser_created, on_browser_created_arg) =
-            anonymize_on_browser_created(on_browser_created);
-        let (get_scale_factor, get_scale_factor_arg) = anonymize_get_scale_factor(get_scale_factor);
-        let (get_screen_point, get_screen_point_arg) = anonymize_get_screen_point(get_screen_point);
+
+        let (get_view_rect, get_view_rect_arg) = anonymize(get_view_rect);
+        let (on_paint, on_paint_arg) = anonymize(on_paint);
+        let (on_browser_created, on_browser_created_arg) = anonymize_mut(on_browser_created);
+        let (get_scale_factor, get_scale_factor_arg) = anonymize(get_scale_factor);
+        let (get_screen_point, get_screen_point_arg) = anonymize(get_screen_point);
+
         let client_settings = sys::ClientSettings {
             get_view_rect: Some(get_view_rect),
             get_view_rect_arg,
@@ -92,86 +96,6 @@ impl CefApp {
 
         receiver.await.expect("browser creation failed")
     }
-}
-
-fn anonymize_on_paint<F: Fn(*const c_void, c_int, c_int)>(
-    f: F,
-) -> (
-    unsafe extern "C" fn(*mut c_void, *const c_void, c_int, c_int),
-    *mut c_void,
-) {
-    let ptr = Box::into_raw(Box::new(f));
-    unsafe extern "C" fn call_thunk<F: Fn(*const c_void, c_int, c_int)>(
-        data: *mut c_void,
-        buf: *const c_void,
-        w: c_int,
-        h: c_int,
-    ) {
-        (*data.cast::<F>())(buf, w, h)
-    }
-    (call_thunk::<F>, ptr.cast())
-}
-
-fn anonymize_get_view_rect<F: Fn(*mut c_int, *mut c_int)>(
-    f: F,
-) -> (
-    unsafe extern "C" fn(*mut c_void, *mut c_int, *mut c_int),
-    *mut c_void,
-) {
-    let ptr = Box::into_raw(Box::new(f));
-    unsafe extern "C" fn call_thunk<F: Fn(*mut c_int, *mut c_int)>(
-        data: *mut c_void,
-        w: *mut c_int,
-        h: *mut c_int,
-    ) {
-        (*data.cast::<F>())(w, h)
-    }
-    (call_thunk::<F>, ptr.cast())
-}
-
-fn anonymize_on_browser_created<F: FnMut(*mut c_void)>(
-    f: F,
-) -> (unsafe extern "C" fn(*mut c_void, *mut c_void), *mut c_void) {
-    let ptr = Box::into_raw(Box::new(f));
-    unsafe extern "C" fn call_thunk<F: FnMut(*mut c_void)>(
-        data: *mut c_void,
-        browser: *mut c_void,
-    ) {
-        (*data.cast::<F>())(browser)
-    }
-    (call_thunk::<F>, ptr.cast())
-}
-
-fn anonymize_get_scale_factor<F: FnMut(*mut c_float)>(
-    f: F,
-) -> (unsafe extern "C" fn(*mut c_void, *mut c_float), *mut c_void) {
-    let ptr = Box::into_raw(Box::new(f));
-    unsafe extern "C" fn call_thunk<F: FnMut(*mut c_float)>(
-        data: *mut c_void,
-        scale_factor: *mut c_float,
-    ) {
-        (*data.cast::<F>())(scale_factor)
-    }
-    (call_thunk::<F>, ptr.cast())
-}
-
-fn anonymize_get_screen_point<F: Fn(c_int, c_int, *mut c_int, *mut c_int)>(
-    f: F,
-) -> (
-    unsafe extern "C" fn(*mut c_void, c_int, c_int, *mut c_int, *mut c_int),
-    *mut c_void,
-) {
-    let ptr = Box::into_raw(Box::new(f));
-    unsafe extern "C" fn call_thunk<F: Fn(c_int, c_int, *mut c_int, *mut c_int)>(
-        data: *mut c_void,
-        x: c_int,
-        y: c_int,
-        screen_x: *mut c_int,
-        screen_y: *mut c_int,
-    ) {
-        (*data.cast::<F>())(x, y, screen_x, screen_y)
-    }
-    (call_thunk::<F>, ptr.cast())
 }
 
 fn get_posix_args() -> (c_int, *mut *mut c_char) {
