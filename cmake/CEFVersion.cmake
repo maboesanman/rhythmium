d5a25e7f0608dd186c1983135ed53fa4c61c6894
Mason Boeman <masonboeman@gmail.com>
2026-05-19 20:20:00 -0500

    another comment

--- cmake/CEFVersion.cmake
+++ cmake/CEFVersion.cmake
@@ -1,3 +1,4 @@
+# This file is parsed by the cef_wrapper build.rs as well as cmake so be careful making structural changes to it.
 
 # Specify the CEF distribution version.
 set(CEF_VERSION "147.0.14+g76d2442+chromium-147.0.7727.138")
